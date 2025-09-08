38eb623ae43e133d86201083c027fc38ff89da92
Daren Hua <59425042+darenhua@users.noreply.github.com>
2025-09-08 11:34:22 -0400

    tanstack start

---
+++ src/routes/index.tsx
@@ -0,0 +1,39 @@
+import { createFileRoute } from "@tanstack/react-router";
+import logo from "../logo.svg";
+import { useQuery } from "@tanstack/react-query";
+
+export const Route = createFileRoute("/")({
+    component: App,
+});
+
+interface Assignment {
+    id: number;
+    title: string;
+    courseName: string;
+    dueDate: string;
+}
+
+function AssignmentListItem({ assignment }: { assignment: Assignment }) {
+    return <div>{assignment.title} - duedate</div>;
+}
+
+function App() {
+    const assignments = useQuery({
+        queryKey: ["assignments"],
+        queryFn: () =>
+            fetch("https://jsonplaceholder.typicode.com/todos").then(
+                (res) => res.json() as Promise<Assignment[]>
+            ),
+    });
+
+    return (
+        <div className="flex flex-col items-center">
+            {assignments.data?.map((assignment) => (
+                <AssignmentListItem
+                    key={assignment.id}
+                    assignment={assignment}
+                />
+            ))}
+        </div>
+    );
+}
